e5721778630d58d34940f74cac240166662b5038
hongwoo-nam <hwnam@cubrid.com>
2017-04-04 20:34:22 +0900

    run indent parser_support.c

--- src/parser/parser_support.c
+++ src/parser/parser_support.c
@@ -10654,7 +10654,7 @@ pt_process_spec_for_update (PARSER_CONTEXT * parser, PT_NODE * spec, PT_NODE * n
 	  return NULL;
 	}
 
-      spec->info.spec.derived_table = save_dt;	  /* moved here from just above to avoid segmentation fault */
+      spec->info.spec.derived_table = save_dt;	/* moved here from just above to avoid segmentation fault */
 
       /* next derived table of union */
       derived_table = (derived_table == dt_arg1 ? dt_arg2 : NULL);
